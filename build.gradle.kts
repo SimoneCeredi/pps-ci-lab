d12d420cd782806483516ea82b73a364d0bc2873
Simone Ceredi <ceredi.simone.iti@gmail.com>
2023-05-02 17:57:02 +0200

    Completed step 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,9 @@ repositories {
 }
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.2.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
 }
---
+++ src/test/scala/ScaloneTest.scala
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class ScaloneTest {
+  @Test
+  def testing():Unit =
+    assertTrue(true)
+}
